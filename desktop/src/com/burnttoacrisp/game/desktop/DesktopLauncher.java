18c458ea43e313749c212de9a100ff230e0c1bc1
Parth Oza <plusparth@gmail.com>
2017-01-31 09:08:51 -0500

    Add LibGDX setup stuff

---
+++ desktop/src/com/burnttoacrisp/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.burnttoacrisp.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.burnttoacrisp.game.GraphicsPlatformer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GraphicsPlatformer(), config);
+	}
+}
---
+++ html/src/com/burnttoacrisp/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.burnttoacrisp.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.burnttoacrisp.game.GraphicsPlatformer;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new GraphicsPlatformer();
+        }
+}
